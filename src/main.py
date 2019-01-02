67f7980b91da73e0691731cffed6f24065357291
Ahmed-Abdullah88 <ahmedbaseer111@gmail.com>
2019-01-02 07:28:08 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+# Finished video No. 2
 
+greeting = 'Hello'
+name = input('Enter your name: ')
+
+# f-strings:
+# Available starting from python 3.6
+# Allows the use of methods inside the place holders as in this example:
+message = f'{greeting}, {name.title()}. Welcome!'
+
+print(message)
+
+print(help(name.lower))
