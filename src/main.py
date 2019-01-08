742d135b106a130a6f88f6bec5d37f08c84cdce3
Ahmed Abdullah <ahmedbaseer111@gmail.com>
2019-01-08 03:54:43 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,8 @@
-# Finished video No. 5
+# Finished video No. 6
 
-student = {'name': 'john', 'age': 25, 'courses': ['Math', 'CompSci']}
+condition = 'Test String'
 
-student.update({'name': 'Jane', 'age': 30, 'email': 'aa@aa.com'})
-# print(student.get('phone', 'Not Found'))
-print(student)
-
-del student['email']
-
-age = student.pop('age')
-print(age)
-print(student)
-print(student.items())
-
-print('*********************')
-
-for key, value in student.items():
-    print(key, value)
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluated to False')
