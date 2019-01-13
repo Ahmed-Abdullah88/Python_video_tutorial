44d7e6cbb172ebefaa882793d0dfa3ef449cb450
Ahmed Abdullah <ahmedbaseer111@gmail.com>
2019-01-13 10:52:52 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
-# Finished video No. 7
+# Finished video No. 8
 
-x = 1
+# number of days per month. First value placeholder for indexing puposes
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-while True:
-    print(x)
-    x += 1
+def is_leap(year):
+    """Return True for leap years, False for non-leap years."""
+    
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def days_in_month(year, month):
+    """Return number of days in that month in that year."""
+
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+    
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+print(is_leap(217), days_in_month(217, 9))
